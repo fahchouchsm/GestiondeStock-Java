8f2ea52b2e72132ac93d6deff7dc6c9cbc427f56
Fahchouch Mohammed <fahchouchsm@gmail.com>
2025-05-20 17:47:45 +0100

    final version

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import navigation.Konsole;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        new DatabaseManager("jdbc:mysql://localhost:3306/GSJava", "root", "");
+        new DatabaseManager("jdbc:mysql://localhost:3306/gsJava", "root", "");
         Konsole.clearConsole();
         Konsole.showNavigator();
     }
